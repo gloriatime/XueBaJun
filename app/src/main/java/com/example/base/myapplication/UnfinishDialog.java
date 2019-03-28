642c087520ee13c9046cbc22e6252ee0e1a5968b
gloriatime <947727923@qq.com>
2019-03-28 15:48:44 +0800

    添加再见功能

--- app/src/main/java/com/example/base/myapplication/UnfinishDialog.java
+++ app/src/main/java/com/example/base/myapplication/UnfinishDialog.java
@@ -1,5 +1,6 @@
 package com.example.base.myapplication;
 
+import android.content.ContentProvider;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
@@ -11,7 +12,7 @@ import org.json.JSONException;
 
 public class UnfinishDialog {
 
-    public UnfinishDialog(Context context){
+    public UnfinishDialog(final Context context){
         // 弹出包含修改删除两种选项的对话框
         AlertDialog dialog = new AlertDialog.Builder(context)
                 .setTitle("非常抱歉，该功能尚在开发中。")//设置对话框的标题
@@ -24,7 +25,22 @@ public class UnfinishDialog {
                 .setNegativeButton("我一点都不期待，嘻嘻。", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        showGoodByeDialog(context);
                         dialog.dismiss();
+                    }
+                })
+                .create();
+        dialog.show();
+    }
+
+    private void showGoodByeDialog(Context context) {
+        AlertDialog dialog = new AlertDialog.Builder(context)
+                .setTitle("再！ 见！")//设置对话框的标题
+                .setPositiveButton("好聚好散", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        android.os.Process.killProcess(android.os.Process.myPid()) ;   //获取PID
+                        System.exit(0);   //常规java、c#的标准退出法，返回值为0代表正常退出
                     }
                 })
                 .create();
