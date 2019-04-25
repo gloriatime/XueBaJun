bce5c92e1c34522095ac6e7c0f3f17a8dc368dc8
CatherineHu123 <15771073635@163.com>
2019-04-25 19:06:58 +0800

    评论回复多条点击

--- app/src/main/java/com/example/gloria/myapplication/showInfo/CourseDetailActivity.java
+++ app/src/main/java/com/example/gloria/myapplication/showInfo/CourseDetailActivity.java
@@ -20,8 +20,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewDebug;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -57,11 +59,14 @@ import org.json.JSONObject;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CourseDetailActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -404,7 +409,11 @@ public class CourseDetailActivity extends AppCompatActivity implements View.OnCl
         Radapter = new ReplyAdapter(replyView.getContext(), replyList);
         ListView replyListView = (ListView)replyView.findViewById(R.id.reply_list);
         replyListView.setAdapter(Radapter);
-        dialog.setContentView(replyView);
+        ViewGroup parent = (ViewGroup)replyView.getParent();
+        if(parent!=null){
+            parent.removeAllViews();
+            Log.e("####","有父布局");
+        }
         /***********
          * 监听回复
          */
@@ -654,4 +663,137 @@ public class CourseDetailActivity extends AppCompatActivity implements View.OnCl
         bj.setBack(back_button);
     }
 
+    //adapter
+    public class CommentAdapter extends BaseAdapter {
+        private List<Comment> commentBeanList;
+        private Context context;
+
+        public CommentAdapter(Context context, List<Comment> commentBeanList) {
+            this.context = context;
+            this.commentBeanList = commentBeanList;
+        }
+
+        @Override
+        public int getCount() {
+            return commentBeanList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return commentBeanList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return commentBeanList.get(position).getId();
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            final CommentHolder commentHolder;
+
+            if (convertView == null) {
+                convertView = LayoutInflater.from(context).inflate(R.layout.comment_item_layout, parent, false);
+                commentHolder = new CommentHolder(convertView);
+                convertView.setTag(commentHolder);
+            } else {
+                commentHolder = (CommentHolder) convertView.getTag();
+            }
+            final RequestQueue mQueue = Volley.newRequestQueue(convertView.getContext());
+            ImageRequest imageRequest = new ImageRequest(
+                    "http://47.100.226.176:8080/XueBaJun/head_image/" + commentBeanList.get(position).getCritic().getPhone() + ".jpg",
+                    new Response.Listener<Bitmap>() {
+                        @Override
+                        public void onResponse(Bitmap response) {
+                            BitmapDrawable temp = new BitmapDrawable(response);
+                            commentHolder.logo.setImageDrawable(temp);
+                        }
+                    }, 300, 300, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    commentHolder.logo.setBackgroundResource(R.drawable.ic_head_image);
+                }
+            });
+            //mQueue.add(imageRequest);
+            commentHolder.tv_name.setText(commentBeanList.get(position).getCritic().getName());
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String date = format.format(commentBeanList.get(position).getDate());
+            commentHolder.tv_time.setText(date);
+            commentHolder.tv_content.setText(commentBeanList.get(position).getContent());
+            commentHolder.tv_reply.setTag(position);
+            commentHolder.tv_reply.setOnClickListener(new MyListener(position));
+
+            return convertView;
+        }
+
+        private class CommentHolder {
+            private CircleImageView logo;
+            private TextView tv_name, tv_content, tv_time;
+            private TextView tv_reply;
+
+            public CommentHolder(View view) {
+                logo = (CircleImageView) view.findViewById(R.id.comment_item_logo);
+                tv_content = (TextView) view.findViewById(R.id.comment_item_content);
+                tv_name = (TextView) view.findViewById(R.id.comment_item_userName);
+                tv_time = (TextView) view.findViewById(R.id.comment_item_time);
+                tv_reply = (TextView) view.findViewById(R.id.comment_item_reply);
+            }
+        }
+
+        class MyListener implements View.OnClickListener {
+            int pos;
+
+            public MyListener(int pos) {
+                this.pos = pos;
+            }
+
+            @Override
+            public void onClick(final View arg0) {
+                String url = "http://47.100.226.176:8080/XueBaJun/GetComment";
+
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("id", commentList.get(pos).getId());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, jsonObject, new Response.Listener<JSONObject>() {
+
+                    public void onResponse(JSONObject jsonObject) {
+                        Gson gson = new DateGson().getGson();
+                        Comment ct = gson.fromJson(jsonObject.toString(), Comment.class);
+                        Log.e("##", "评论是否有回复" + ct.getReplyList().size());
+                        if (ct.getReplyList().size() == 0) {
+                            Log.e("##", "firstreply");
+                            addFirstReply(pos);
+                        } else {
+                            replyList = ct.getReplyList();
+                            Log.e("##########", "查看回复查看查看");
+                            Log.e("####reply_pos", "" + pos);
+                            showReplyDetail(pos);
+                            Log.e("###", "准备进入回复界面啦啦啦啦");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                    }
+                });
+                mQueue.add(jsonObjectRequest);
+            }
+        }
+
+        /********
+         * 插入一条新的评论
+         * @param commentDetailBean
+         */
+        public void addTheCommentData(Comment commentDetailBean) {
+            if (commentDetailBean != null) {
+                commentDetailBean.setReplyList(null);
+                commentBeanList.add(commentDetailBean);
+                notifyDataSetChanged();
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/gloria/myapplication/showInfo/UserInfoActivity.java
+++ app/src/main/java/com/example/gloria/myapplication/showInfo/UserInfoActivity.java
@@ -3,6 +3,8 @@ package com.example.gloria.myapplication.showInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.gloria.myapplication.R;
+
 public class UserInfoActivity extends AppCompatActivity {
 
     @Override
